adae6bc347d1a774b5e32fdb7c7f3c5c32bce252
KaitoRyouga <kaito1477800@gmail.com>
2020-06-27 11:30:25 +0700

    fix

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
+export default home;
